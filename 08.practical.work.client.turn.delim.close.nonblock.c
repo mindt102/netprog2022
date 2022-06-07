b40b8ab4db3150d7870bd4c9e62c054572877dba
mindt102 <minhdt24082002@gmail.com>
2022-06-07 11:24:59 +0000

    finish pw08

---
+++ 08.practical.work.client.turn.delim.close.nonblock.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <unistd.h>
+
+#define MAX_LENGTH 255
+#define PORT 8784
+
+int main(int argc, char **argv)
+{
+    setbuf(stdout, NULL);
+
+    struct sockaddr_in saddr;
+    int sockfd;
+
+    char hostname[MAX_LENGTH];
+    if (argc > 1)
+    {
+        strncpy(hostname, argv[1], MAX_LENGTH);
+    }
+    else
+    {
+        printf("Enter a hostname: ");
+        scanf("%255s", hostname);
+
+        // Clear input buffer
+        // https://www.geeksforgeeks.org/clearing-the-input-buffer-in-cc/
+        while ((getchar()) != '\n')
+            ; // I try to fix this but VSCode auto format feature hates me
+    }
+
+    struct hostent *host_ptr;
+    if ((host_ptr = gethostbyname(hostname)) == NULL)
+    {
+        perror("Error Resolving.\n");
+        return 1;
+    }
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Error creating socket.\n");
+        return 1;
+    }
+
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    memcpy((char *)&saddr.sin_addr.s_addr, host_ptr->h_addr_list[0], host_ptr->h_length);
+    saddr.sin_port = htons(PORT);
+
+    setsockopt(sockfd, SOL_SOCKET,
+               SO_REUSEADDR, &(int){1},
+               sizeof(int));
+
+    if (connect(sockfd, (struct sockaddr *)&saddr, sizeof(saddr)) < 0)
+    {
+        perror("Error connecting.\n");
+        return 1;
+    }
+
+    int sockfl = fcntl(sockfd, F_GETFL, 0);
+    sockfl |= O_NONBLOCK;
+    fcntl(sockfd, F_SETFL, sockfl);
+
+    struct pollfd stdin_pollfd;
+    stdin_pollfd.fd = fileno(stdin);
+    stdin_pollfd.events = POLLIN;
+
+    char ip_addr[32];
+    printf("Connected to %s\n> ", inet_ntop(host_ptr->h_addrtype, host_ptr->h_addr_list[0], ip_addr, sizeof(ip_addr)));
+
+    char message[1024];
+
+    int connected = 1;
+    while (connected)
+    {
+        if (recv(sockfd, message, sizeof(message), 0) > 0)
+        {
+            printf("\rServer: ");
+            printf("%s", message);
+            if (strcmp(message, "Disconnect\n") == 0)
+            {
+                break;
+            }
+            else if (strcmp(message, "/clear\n") == 0)
+            {
+                system("clear");
+                printf("> ");
+                continue;
+            }
+
+            while (message[strlen(message) - 1] != '\n')
+            {
+                recv(sockfd, message, sizeof(message), 0);
+                printf("%s", message);
+            }
+            printf("> ");
+        }
+
+        if (poll(&stdin_pollfd, 1, 0) > 0)
+        {
+            if (stdin_pollfd.revents & POLLIN)
+            {
+                fgets(message, sizeof(message), stdin);
+                if (strcmp(message, "/quit\n") == 0)
+                {
+                    strcpy(message, "Disconnect\n");
+                    printf("Disconnected.\n");
+                    connected = 0;
+                }
+
+                send(sockfd, message, strlen(message) + 1, 0);
+                while (message[strlen(message) - 1] != '\n')
+                {
+                    fgets(message, sizeof(message), stdin);
+                    send(sockfd, message, strlen(message) + 1, 0);
+                }
+
+                if (connected)
+                {
+                    printf("> ");
+                }
+            }
+        }
+    }
+    close(sockfd);
+    return 0;
+}
---
+++ 08.practical.work.server.turn.delim.close.nonblock.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <unistd.h>
+
+int main()
+{
+    setbuf(stdout, NULL);
+
+    int sockfd, clen, clientfd;
+    struct sockaddr_in saddr, caddr;
+    unsigned short port = 8784;
+    clen = sizeof(caddr);
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("Error creating socket\n");
+        return 1;
+    }
+
+    setsockopt(sockfd, SOL_SOCKET,
+               SO_REUSEADDR, &(int){1},
+               sizeof(int));
+
+    int sockfl = fcntl(sockfd, F_GETFL, 0);
+    sockfl |= O_NONBLOCK;
+    fcntl(sockfd, F_SETFL, sockfl);
+
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    saddr.sin_port = htons(port);
+
+    if (bind(sockfd, (struct sockaddr *)&saddr, sizeof(saddr)) < 0)
+    {
+        perror("Error binding");
+        return 1;
+    }
+
+    if (listen(sockfd, 5) < 0)
+    {
+        perror("Error listening");
+        return 1;
+    }
+
+    struct pollfd stdin_pollfd;
+    stdin_pollfd.fd = fileno(stdin);
+    stdin_pollfd.events = POLLIN;
+
+    int running = 1;
+    printf("Listening on port %d...\n", port);
+    while (running)
+    {
+        clientfd = accept(sockfd, (struct sockaddr *)&caddr, &clen);
+        if (clientfd > 0)
+        {
+            printf("A client connected!\n");
+
+            int clientfl = fcntl(clientfd, F_GETFL, 0);
+            clientfl |= O_NONBLOCK;
+            fcntl(clientfd, F_SETFL, clientfl);
+
+            char message[1024];
+
+            printf("> ");
+            int connected = 1;
+            while (connected)
+            {
+                if (recv(clientfd, message, sizeof(message), 0) > 0)
+                {
+                    printf("\rClient: ");
+                    printf("%s", message);
+                    if (strcmp(message, "Disconnect\n") == 0)
+                    {
+                        break;
+                    }
+
+                    while (message[strlen(message) - 1] != '\n')
+                    {
+                        recv(clientfd, message, sizeof(message), 0);
+                        printf("%s", message);
+                    }
+
+                    printf("> ");
+                }
+
+                if (poll(&stdin_pollfd, 1, 0) > 0)
+                {
+                    if (stdin_pollfd.revents & POLLIN)
+                    {
+                        fgets(message, sizeof(message), stdin);
+                        if (strcmp(message, "/dc\n") == 0)
+                        {
+                            strcpy(message, "Disconnect\n");
+                            printf("Disconnected.\n");
+                            connected = 0;
+                        }
+                        else if (strcmp(message, "/quit\n") == 0)
+                        {
+                            strcpy(message, "Disconnect\n");
+                            printf("Disconnected.\n");
+                            connected = 0;
+                            running = 0;
+                        }
+                        else if (strcmp(message, "/clear\n") == 0)
+                        {
+                            system("clear");
+                            printf("> ");
+                            continue;
+                        }
+
+                        send(clientfd, message, strlen(message) + 1, 0);
+                        while (message[strlen(message) - 1] != '\n')
+                        {
+                            fgets(message, sizeof(message), stdin);
+                            send(clientfd, message, strlen(message) + 1, 0);
+                        };
+
+                        if (connected)
+                        {
+                            printf("> ");
+                        }
+                    }
+                }
+            }
+            close(clientfd);
+            if (running)
+            {
+                printf("Listening on port %d...\n", port);
+            }
+        }
+    }
+    close(sockfd);
+    return 0;
+}
